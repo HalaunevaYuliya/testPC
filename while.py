914cd7698463f1e6a3d2b494a2a711820f21e54c
HalaunevaYuliya <lulushkatutby@gmail.com>
2021-08-31 23:30:19 +0300

    calculator apdate Cannot be divided by 0

--- while.py
+++ while.py
@@ -1,17 +1,20 @@
 print('It is simple calculator')
 x = 'start'
+
 while x != '=':
     a = float(input('enter the number: '))
     x = input('choose operator: (+, -. *, / or =)')
     b = float(input('enter next number: '))
     if x == '+':
-        #res = res
         print('result: ', a, '+', b, '=', a + b)
     elif x == '-':
         print('result: ', a, '-', b, '=', a - b)
     elif x == '*':
         print('result: ', a, '*', b, '=', a * b)
     elif x == '/':
-        print('result: ', a, '/', b, '=', a / b)
+        if b != 0:
+            print('result: ', a, '/', b, '=', a / b)
+        else:
+            print('ERROR! Cannot be divided by 0')
 
 print('Thank you!!!')
