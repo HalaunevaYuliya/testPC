344c22ad5026bf8aae7cb331c245ea708b7229b3
HalaunevaYuliya <lulushkatutby@gmail.com>
2021-09-01 20:56:16 +0300

    error removal

--- calculator.py
+++ calculator.py
@@ -1,17 +1,18 @@
-print('It is simple calculator')
+print('It is simple calculator') # without cycle
 x = 'start'
 while x != '=':
     a = float(input('enter the number: '))
     x = input('choose operator: (+, -. *, / or =)')
     b = float(input('enter next number: '))
     if x == '+':
-        #res = res
         print('result: ', a, '+', b, '=', a + b)
     elif x == '-':
         print('result: ', a, '-', b, '=', a - b)
     elif x == '*':
         print('result: ', a, '*', b, '=', a * b)
-    elif x == '/':
-        print('result: ', a, '/', b, '=', a / b)
-
+        if b != 0:
+            res = a / b
+            print('result: ', a, '/', b, '=', a / b)
+        else:
+            print('ERROR! Cannot be divided by 0')
 print('Thank you!!!')
--- calculator_last_version.py
+++ calculator_last_version.py
@@ -1,20 +1,24 @@
-print('It is simple calculator')
-x = 'start'
+print('It is simple calculator')  # program with zero
 
+a = float(input('enter the number: '))
+x = input('choose operator: (+, -. *, / or =)')
 while x != '=':
-    a = float(input('enter the number: '))
-    x = input('choose operator: (+, -. *, / or =)')
     b = float(input('enter next number: '))
     if x == '+':
+        res = a + b
         print('result: ', a, '+', b, '=', a + b)
     elif x == '-':
+        res = a - b
         print('result: ', a, '-', b, '=', a - b)
     elif x == '*':
+        res = a * b
         print('result: ', a, '*', b, '=', a * b)
     elif x == '/':
         if b != 0:
+            res = a / b
             print('result: ', a, '/', b, '=', a / b)
         else:
             print('ERROR! Cannot be divided by 0')
-
+    a = res
+    x = input('choose operator: (+, -. *, / or =)')
 print('Thank you!!!')
