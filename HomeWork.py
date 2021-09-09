9c8083a4856c3e07a7c45ac8674638fb0eaa1770
HalaunevaYuliya <lulushkatutby@gmail.com>
2021-09-09 15:09:47 +0300

    добавлен просмотр отдельных словарей из общего

--- HomeWork.py
+++ HomeWork.py
@@ -29,12 +29,13 @@ training_dict = {'day1':day_1, 'day2':day_2, 'day3':day_3}
 print('План тренировки в тренажерном зале, расчитанный на 3 тренировки в неделю')
 print('1 - Просмотр недельного плана')
 print('2 - Добавить дополнительную тренировку в бассейне')
-print('3 - Удалить тренировочный день')
+print('3 - Удалить тренировку')
 print('4 - Вывести отдельный тренировочный день')
+print('5 - Отметить завершенную тренировку')
 print('0 - Для выхода из программы')
 x = ''
 while x != 0:
-        x = int(input('Сделайте выбор: 1, 2, 3, 4 или 0  '))
+        x = int(input('Сделайте выбор: 1, 2, 3, 4 или 0 - '))
         if x == 1:
                 for i in training_dict:
                         print(f'{i}')
@@ -42,6 +43,40 @@ while x != 0:
                                print(f'{ii} - {training_dict[i][ii]}')
         elif x == 2:
                 training_dict['day_4'] = {'swimming': '35 min'}
+                x = int(input('посмотрите добавление по команде 1: '))
+                if x == 1:
+                        for i in training_dict:
+                                print(f'{i}')
+                                for ii in training_dict[i]:
+                                        print(f'{ii} - {training_dict[i][ii]}')
+        elif x == 3:    # реализовано неверно
+                a = int(input('выбрать для удаления день 1,2 или 3 -'))
+                if a == 1:
+                        training_dict = training_dict.pop('day1')
+                elif a == 2:
+                        training_dict = training_dict.pop('day2')
+                elif a == 3:
+                        training_dict = training_dict.pop('day3')
+                x = int(input('просмотр перечня после удаления -> 1: '))
+                if x == 1:
+                        for i in training_dict:
+                                print(f'{i}')
+        elif x == 4:
+                x = int(input('Выберите день для просмотра упражнений: 1, 2, 3 или 0 - '))
+                import yaml
+                if x == 1:
+                        d = day_1
+                        print(yaml.dump(d))
+                elif x == 2:
+                        d = day_2
+                        print(yaml.dump(d))
+                elif x == 3:
+                        d = day_3
+                        print(yaml.dump(d))
+
+
+
+
 
 
 
